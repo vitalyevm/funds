03165c1d1ae1bf0a648654f27874d9b72dadfedb
vitalyevm <xcube.studio248@gmail.com>
2023-12-14 15:44:11 +0700

    init

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -15,8 +15,7 @@ async function main() {
   console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
